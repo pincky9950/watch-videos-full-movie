de429f3b01368f830804ed353a0e2a8b916f3de6
movie download <137373820+pincky9950@users.noreply.github.com>
2025-12-24 18:41:18 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h2>Facebook Preview Redirect Service ✅</h2>
+      <p>System is active.</p>
+    </main>
+  );
+}
